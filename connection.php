a55eaf0d2401c990bc141393fab175bac29ca255
Natrajan Jaiswal <natrajan.jaiswal@natrajan-mac.local>
2020-07-10 10:45:30 +0530

    prod db

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php 
 
-$con=mysqli_connect("localhost","root","","shaadikatime");
+$con=mysqli_connect("localhost","u338198937_root","May@2020","u338198937_shaadikatime");
 
 		if(mysqli_connect_errno())
 		{
